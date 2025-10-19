c8f733d6f53c791105462777d3aa266cd293dde4
EdenReuven <edenreuven14@gmail.com>
2025-10-19 08:38:33 +0300

    refactor tsconfig

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {} from '@angular/google-maps';
-import { environment } from '../../environments/environment';
+import { environment } from '@environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MapService {
